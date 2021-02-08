c9e6005b16025214bc524b257df3158791d21226
irubido <ivan.rubido@gmail.com>
2021-02-08 11:21:17 +0100

    crete list element draft

--- src/js/libs/components/fetch.js
+++ src/js/libs/components/fetch.js
@@ -6,7 +6,7 @@ function append(parent, el) {
   return parent.appendChild(el);
 }
 
-const ul = document.getElementById('TODO-ul-ID');
+const ul = document.getElementById('metricsData');
 
 export function getTableData() {
     const response = fetch("https://westend.vedran.nodefactory.io/metrics")
@@ -15,6 +15,17 @@ export function getTableData() {
         const activeNodes =  Number(/vedran_number_of_active_nodes\s*(\d*)\n/g.exec(text)[1]);
         const penalizedNodes =  Number(/vedran_number_of_penalized_nodes\s*(\d*)\n/g.exec(text)[1]);
         const totalNodes = activeNodes + penalizedNodes;
+        let li = createNode('li');
+        let activeNodesSpan = createNode('span');
+        let penalizedNodesSpan = createNode('span');
+        let totalNodesSpan = createNode('span');
+        activeNodesSpan.innerHTML = `Active nodes ${activeNodes}`;
+        penalizedNodesSpan.innerHTML = `Penalized nodes ${penalizedNodes}`;
+        totalNodesSpan.innerHTML = `Total nodes ${totalNodes}`;
+        append(li, activeNodesSpan);
+        append(li, penalizedNodesSpan);
+        append(li, totalNodesSpan);
+        append(ul, li);
         console.log(activeNodes, penalizedNodes, totalNodes)
     })
 }
